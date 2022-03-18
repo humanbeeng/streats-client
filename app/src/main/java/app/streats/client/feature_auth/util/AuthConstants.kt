f89031f72131d4aced88a215491594ac53e4d18e
Nithin SJ <thenithinraj@gmail.com>
2022-03-18 17:23:20 +0530

    Add AuthConstants - Pref names - HTTP failure message

--- app/src/main/java/app/streats/client/feature_auth/util/AuthConstants.kt
+++ app/src/main/java/app/streats/client/feature_auth/util/AuthConstants.kt
@@ -3,5 +3,7 @@ package app.streats.client.feature_auth.util
 object AuthConstants {
     const val SERVER_CLIENT_ID = "107493273096403048465"
     const val ERROR_STRING = "Uh oh! Something went wrong while trying to sign you in 🤥"
-    const val GOOGLE_SIGN_IN_TOKEN_STRING = "GoogleSignInToken"
+    const val GOOGLE_SIGN_IN_TOKEN_PREF = "GoogleSignInToken"
+    const val ACCESS_TOKEN_PREF = "AccessToken"
+    const val AUTH_REQUEST_FAILURE = "Login request failed"
 }
