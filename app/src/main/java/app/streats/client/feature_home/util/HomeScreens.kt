23cde3e4126a317eacee71c95637144082339268
Nithin SJ <thenithinraj@gmail.com>
2022-03-25 23:15:36 +0530

    Add HomeScreens

--- app/src/main/java/app/streats/client/feature_home/util/HomeScreens.kt
+++ app/src/main/java/app/streats/client/feature_home/util/HomeScreens.kt
@@ -2,4 +2,5 @@ package app.streats.client.feature_home.util
 
 sealed class HomeScreens(val route: String) {
     object HomeScreen : HomeScreens("home_screen")
+    object ShopScreen : HomeScreens("shop_screen")
 }
