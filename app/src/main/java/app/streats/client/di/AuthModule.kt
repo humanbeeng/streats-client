444bb61888c5664cd0014dc0767364cb44d24392
Nithin SJ <thenithinraj@gmail.com>
2022-03-25 23:10:51 +0530

    Move AuthApi provider to AuthModule

--- app/src/main/java/app/streats/client/di/AuthModule.kt
+++ app/src/main/java/app/streats/client/di/AuthModule.kt
@@ -1,10 +1,16 @@
 package app.streats.client.di
 
+import app.streats.client.core.util.Constants
+import app.streats.client.feature_auth.data.AuthApi
 import com.google.firebase.auth.FirebaseAuth
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -15,5 +21,21 @@ object AuthModule {
     @Singleton
     fun providesFirebaseAuth(): FirebaseAuth {
         return FirebaseAuth.getInstance()
+    }
+
+    private val httpLoggingInterceptor =
+        HttpLoggingInterceptor().setLevel(level = HttpLoggingInterceptor.Level.BASIC)
+
+    private val okHttpClient = OkHttpClient.Builder().addInterceptor(httpLoggingInterceptor).build()
+
+    @Singleton
+    @Provides
+    fun provideAuthApi(): AuthApi {
+        return Retrofit.Builder()
+            .client(okHttpClient)
+            .baseUrl(Constants.SERVER_BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(AuthApi::class.java)
     }
 }
