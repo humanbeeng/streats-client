c35ae0618fa197b3129707b7c97ef0042ecca2c5
Nithin SJ <thenithinraj@gmail.com>
2022-04-01 20:42:15 +0530

    Add CartConstants with Error Message

---
+++ app/src/main/java/app/streats/client/feature_cart/util/CartConstants.kt
@@ -0,0 +1,8 @@
+package app.streats.client.feature_cart.util
+
+/**
+ * TODO : Refactor common error message in core constants
+ */
+object CartConstants {
+    const val CART_ERROR_MESSAGE = "Uh oh! Something went wrong 🤥"
+}
