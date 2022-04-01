841e19c6825aaa1d92699e59dd0118f2ce8dfddb
Nithin SJ <thenithinraj@gmail.com>
2022-04-01 10:26:10 +0530

    Add Shimmer Library

--- app/src/main/java/app/streats/client/feature_home/presentation/HomeScreen.kt
+++ app/src/main/java/app/streats/client/feature_home/presentation/HomeScreen.kt
@@ -3,6 +3,7 @@ package app.streats.client.feature_home.presentation
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Button
 import androidx.compose.material.Card
 import androidx.compose.material.Surface
@@ -12,6 +13,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+
+/**
+ * TODO : Add Shimmer Cards while loading
+ */
 
 @Composable
 fun HomeScreen(homeScreenViewModel: HomeScreenViewModel = hiltViewModel()) {
@@ -32,14 +37,19 @@ fun HomeScreen(homeScreenViewModel: HomeScreenViewModel = hiltViewModel()) {
             val username = homeScreenState.data?.username
             Text(text = username.toString())
 
-            LazyColumn(modifier = Modifier.fillMaxWidth()) {
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
                 homeScreenState.data?.nearbyShops?.let { it ->
                     items(it) {
-
                         Card(
                             modifier = Modifier
                                 .fillMaxWidth()
-                                .padding(16.dp)
+                                .height(100.dp)
+                                .padding(6.dp),
+                            shape = RoundedCornerShape(8.dp),
+                            elevation = 4.dp
                         ) {
                             Text(text = it.shopName)
 
