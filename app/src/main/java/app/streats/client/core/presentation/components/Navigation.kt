8a8f9d030b5b6fb1345fc52ebedd8cdd3505c485
Nithin SJ <thenithinraj@gmail.com>
2022-04-01 20:39:17 +0530

    Revert start destination to SplashScreen

--- app/src/main/java/app/streats/client/core/presentation/components/Navigation.kt
+++ app/src/main/java/app/streats/client/core/presentation/components/Navigation.kt
@@ -4,18 +4,18 @@ import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import app.streats.client.core.presentation.CartScreen
 import app.streats.client.core.presentation.OrderScreen
 import app.streats.client.feature_auth.presentation.login_screen.LoginScreen
 import app.streats.client.feature_auth.presentation.splash_screen.SplashScreen
 import app.streats.client.feature_auth.util.AuthScreens
+import app.streats.client.feature_cart.presentation.cart_screen.CartScreen
 import app.streats.client.feature_home.presentation.HomeScreen
 import app.streats.client.feature_home.util.HomeScreens
 
 @Composable
 fun Navigation(navController: NavHostController) {
 
-    NavHost(navController = navController, startDestination = HomeScreens.HomeScreen.route) {
+    NavHost(navController = navController, startDestination = AuthScreens.SplashScreen.route) {
 
         composable(AuthScreens.SplashScreen.route) {
             SplashScreen(
