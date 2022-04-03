753f5240a29a9bc0b5ba7f56ddb7d2704b882bab
Nithin SJ <thenithinraj@gmail.com>
2022-04-03 10:59:23 +0530

    Add RequestBody for add and remove from cart functions

--- app/src/main/java/app/streats/client/feature_cart/data/CartApi.kt
+++ app/src/main/java/app/streats/client/feature_cart/data/CartApi.kt
@@ -1,7 +1,8 @@
 package app.streats.client.feature_cart.data
 
 import app.streats.client.feature_cart.data.dto.CartDTO
-import retrofit2.http.DELETE
+import app.streats.client.feature_cart.data.dto.CartRequest
+import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.POST
@@ -12,8 +13,18 @@ interface CartApi {
     suspend fun cart(@Header("Authorization") accessToken: String): CartDTO
 
     @POST("/cart")
-    suspend fun addToCart(@Header("Authorization") accessToken: String): CartDTO
+    suspend fun addToCart(
+        @Header("Authorization")
+        accessToken: String,
+        @Body
+        request: CartRequest
+    ): CartDTO
 
-    @DELETE("/cart")
-    suspend fun removeFromCart(@Header("Authorization") accessToken: String): CartDTO
+    @POST("/cart/remove")
+    suspend fun removeFromCart(
+        @Header("Authorization")
+        accessToken: String,
+        @Body
+        request: CartRequest
+    ): CartDTO
 }
