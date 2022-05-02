50ee12b2f2b66a44644063fef545a79cf455f7a9
Nithin SJ <thenithinraj@gmail.com>
2022-05-02 11:06:20 +0530

    Refactor AuthApi
    - Updates routes for login and auth

--- app/src/main/java/app/streats/client/feature_auth/data/AuthApi.kt
+++ app/src/main/java/app/streats/client/feature_auth/data/AuthApi.kt
@@ -1,13 +1,29 @@
 package app.streats.client.feature_auth.data
 
-import app.streats.client.feature_auth.data.dto.VerifyRequest
-import app.streats.client.feature_auth.data.dto.VerifyResponse
+import app.streats.client.feature_auth.data.dto.AuthRequestDTO
+import app.streats.client.feature_auth.data.dto.LoginRequestDTO
+import app.streats.client.feature_auth.data.dto.LoginResponseDTO
+import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
+/**
+ * Refactor Login to return Response<LoginResponseDTO>
+ */
 interface AuthApi {
 
+//    @POST("/auth/login")
+//    suspend fun login(@Body loginRequestDTO: LoginRequestDTO): Response<LoginResponseDTO>
+
     @POST("/auth")
-    suspend fun verify(@Body verifyRequest: VerifyRequest): VerifyResponse
+    suspend fun authenticate(
+        @Header("Authorization") accessToken: String, @Body authRequestDTO: AuthRequestDTO
+    ): Response<Unit>
+
+
+    @POST("/auth/login")
+    suspend fun login(@Body loginRequestDTO: LoginRequestDTO): LoginResponseDTO
+
 
 }
