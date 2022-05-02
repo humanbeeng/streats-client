9339ed7682466b3bb10edc92917dbaee7cc0a06a
Nithin SJ <thenithinraj@gmail.com>
2022-05-02 21:07:41 +0530

    Refactor Login method
    - Added FirebaseAuth and CurrentLocationCoordinates as DI

--- app/src/main/java/app/streats/client/feature_auth/presentation/login_screen/LoginScreenViewModel.kt
+++ app/src/main/java/app/streats/client/feature_auth/presentation/login_screen/LoginScreenViewModel.kt
@@ -6,9 +6,12 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import app.streats.client.core.presentation.events.UIEvent
+import app.streats.client.core.util.Constants.FCM_TOKEN_PREF
 import app.streats.client.core.util.Resource
 import app.streats.client.feature_auth.data.repository.AuthRepository
+import app.streats.client.feature_auth.domain.models.CurrentLocationCoordinates
 import app.streats.client.feature_auth.util.AuthConstants
+import app.streats.client.feature_auth.util.AuthConstants.GOOGLE_SIGN_IN_TOKEN_PREF
 import app.streats.client.feature_home.util.HomeScreens
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.common.api.ApiException
@@ -18,14 +21,24 @@ import com.google.firebase.auth.GoogleAuthProvider
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
 
+
+/**
+ * TODO : Refactor login method
+ * TODO : Handle server down. i.e socketConnectionException
+ */
+
 @HiltViewModel
 class LoginScreenViewModel @Inject constructor(
     private val authRepository: AuthRepository,
-    private val sharedPreferences: SharedPreferences
+    private val sharedPreferences: SharedPreferences,
+    private val firebaseAuth: FirebaseAuth,
+    private val currentLocationCoordinates: CurrentLocationCoordinates
 ) : ViewModel() {
 
     private val _outgoingEventFlow = MutableSharedFlow<UIEvent>()
@@ -54,8 +67,15 @@ class LoginScreenViewModel @Inject constructor(
 
     }
 
+    /*
+     * TODO : Refactor this method
+     */
 
     private fun login(task: Task<GoogleSignInAccount>?) {
+
+
+        val fcmToken = sharedPreferences.getString(FCM_TOKEN_PREF, "") ?: ""
+
         val googleSignInAccount = task?.getResult(ApiException::class.java)
         if (googleSignInAccount != null) {
             val idToken = task.result.idToken!!
@@ -70,20 +90,35 @@ class LoginScreenViewModel @Inject constructor(
 
                                 if (idTokenTask.isSuccessful) {
                                     val token = idTokenTask.result.token!!
-                                    viewModelScope.launch {
-                                        when (authRepository.verifyToken(token)) {
+                                    authRepository.login(
+                                        currentLocationCoordinates,
+                                        fcmToken,
+                                        token
+                                    ).onEach { state ->
+                                        when (state) {
                                             is Resource.Error -> {
                                             }
                                             is Resource.Success -> {
-                                                _outgoingEventFlow.emit(UIEvent.Navigate(HomeScreens.HomeScreen.route))
+                                                sharedPreferences.edit()
+                                                    .putString(
+                                                        GOOGLE_SIGN_IN_TOKEN_PREF,
+                                                        state.data
+                                                    ).apply()
+                                                _outgoingEventFlow.emit(
+                                                    UIEvent.Navigate(
+                                                        HomeScreens.HomeScreen.route
+                                                    )
+                                                )
                                             }
                                             is Resource.Loading -> {
 
+                                                Timber.d("Sign in with credentials success. ")
                                             }
                                         }
-                                    }
 
-                                    Timber.d("Sign in with credentials success. ")
+                                    }.launchIn(viewModelScope)
+
+
                                 } else {
                                     sharedPreferences.edit()
                                         .putString(AuthConstants.GOOGLE_SIGN_IN_TOKEN_PREF, "")
@@ -93,15 +128,18 @@ class LoginScreenViewModel @Inject constructor(
                                 }
                             }
                     } else {
-                        sharedPreferences.edit()
-                            .putString(AuthConstants.GOOGLE_SIGN_IN_TOKEN_PREF, "").apply()
-                        Timber.e("Sign in with credentials failed!")
+
                     }
 
                 }
         } else {
-
+//            TODO : Set value to AccessToken DI instead of sharedPref
+            sharedPreferences.edit()
+                .putString(AuthConstants.GOOGLE_SIGN_IN_TOKEN_PREF, "").apply()
+            Timber.e("Sign in with credentials failed!")
         }
+
+
     }
 
     private fun logout() {
