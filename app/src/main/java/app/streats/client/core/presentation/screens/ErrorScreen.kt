b81ad3aa552ff9eda21a8d008cf60b3d1b96b860
Nithin SJ <thenithinraj@gmail.com>
2022-05-02 11:02:47 +0530

    Add ErrorScreen

---
+++ app/src/main/java/app/streats/client/core/presentation/screens/ErrorScreen.kt
@@ -0,0 +1,31 @@
+package app.streats.client.core.presentation.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ErrorScreen() {
+    Scaffold(modifier = Modifier.fillMaxSize()) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+        ) {
+
+            Text(text = "Error Screen", style = TextStyle(fontSize = 16.sp))
+
+
+        }
+
+    }
+}
