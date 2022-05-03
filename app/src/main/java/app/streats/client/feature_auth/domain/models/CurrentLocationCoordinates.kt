f9746926ad41186bb5407ad92b496a731a05552c
Nithin SJ <thenithinraj@gmail.com>
2022-05-03 12:41:23 +0530

    Add default values for Longitude and Latitude

--- app/src/main/java/app/streats/client/feature_auth/domain/models/CurrentLocationCoordinates.kt
+++ app/src/main/java/app/streats/client/feature_auth/domain/models/CurrentLocationCoordinates.kt
@@ -1,6 +1,6 @@
 package app.streats.client.feature_auth.domain.models
 
 data class CurrentLocationCoordinates(
-    var latitude: Double,
-    var longitude: Double
+    var latitude: Double = 0.00,
+    var longitude: Double = 0.00
 )
