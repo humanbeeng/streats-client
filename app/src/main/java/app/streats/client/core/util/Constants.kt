bd27fa131d9ed88745c582a81479ddb8f42c2eb5
Nithin SJ <thenithinraj@gmail.com>
2022-05-03 14:02:39 +0530

    Update Server Base URL to ELB

--- app/src/main/java/app/streats/client/core/util/Constants.kt
+++ app/src/main/java/app/streats/client/core/util/Constants.kt
@@ -4,6 +4,8 @@ object Constants {
 
     const val SERVER_BASE_URL = "http://streats-backend-prod.ap-south-1.elasticbeanstalk.com/"
 
+//    const val SERVER_BASE_URL = "http://10.0.0.2:8080/"
+
     const val ERROR_MESSAGE = "Uh oh! Something went wrong 🤥"
 
     const val FCM_TOKEN_PREF = "FCMToken"
