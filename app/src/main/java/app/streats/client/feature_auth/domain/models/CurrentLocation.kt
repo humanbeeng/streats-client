d2482a44089cc919c9d05fb958bc99397e0d4983
Nithin SJ <thenithinraj@gmail.com>
2022-06-16 08:54:26 +0530

    SA-28 Refactor Login flow to reflect changes in CurrentLocation

--- app/src/main/java/app/streats/client/feature_auth/domain/models/CurrentLocation.kt
+++ app/src/main/java/app/streats/client/feature_auth/domain/models/CurrentLocation.kt
@@ -1,6 +1,19 @@
 package app.streats.client.feature_auth.domain.models
 
+import app.streats.client.core.util.CoreConstants.EMPTY
+
+data class CurrentLocation(
+    var latitude: Double = 0.00,
+    var longitude: Double = 0.00,
+    var subLocality: String = EMPTY
+) {
+    fun toCurrentLocationCoordinates(): CurrentLocationCoordinates {
+        return CurrentLocationCoordinates(latitude, longitude)
+    }
+}
+
+
 data class CurrentLocationCoordinates(
     var latitude: Double = 0.00,
-    var longitude: Double = 0.00
+    var longitude: Double = 0.00,
 )
