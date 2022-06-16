b9a731bc90799f548593ebab9b50334501a06fe7
Nithin SJ <thenithinraj@gmail.com>
2022-06-16 08:53:26 +0530

    FIx fullscreen issues

--- app/src/main/java/app/streats/client/core/presentation/MainActivity.kt
+++ app/src/main/java/app/streats/client/core/presentation/MainActivity.kt
@@ -4,10 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Surface
+import androidx.compose.material.DrawerValue
+import androidx.compose.material.rememberDrawerState
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.navigation.compose.rememberNavController
 import app.streats.client.core.presentation.components.Navigation
 import app.streats.client.core.presentation.ui.theme.StreatsTheme
@@ -17,36 +18,32 @@ import dagger.hilt.android.AndroidEntryPoint
 /**
  * TODO : Resolve navigation issue of BottomNavBar
  *
- * TODO : Remove AppBar from application
+ * TODO : Remove AppBar from applicationA
  */
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-//        WindowCompat.setDecorFitsSystemWindows(window, false)
-//        window.setFlags(
-//            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-//            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
-//        )
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
         setContent {
             StreatsTheme {
                 // A surface container using the 'background' color from the theme
                 val navController = rememberNavController()
                 val interactionSource = remember { MutableInteractionSource() }
-                Navigation(
-                    navController = navController
-                )
-                Surface(
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    Navigation(navController = navController)
-                }
+
+                val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+
+                Navigation(navController = navController)
+
 
             }
+
+
         }
     }
 }
+
 
 
