2e57001bbf4cef80ad7e85f49c83f927ca6cdf61
Nithin SJ <thenithinraj@gmail.com>
2022-06-16 08:55:06 +0530

    SA-28 Add TODO : Adding sublocality field in user

--- app/src/main/java/app/streats/client/feature_auth/data/dto/LoginRequestDTO.kt
+++ app/src/main/java/app/streats/client/feature_auth/data/dto/LoginRequestDTO.kt
@@ -2,6 +2,7 @@ package app.streats.client.feature_auth.data.dto
 
 import app.streats.client.feature_auth.domain.models.CurrentLocationCoordinates
 
+//TODO : Add Sublocality in user
 data class LoginRequestDTO(
     val currentLocationCoordinates: CurrentLocationCoordinates,
     val fcmToken: String,
